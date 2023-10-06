e64c2dcc57c9d2c2fc2b70930ccf2c4baf6e30c6
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-06 17:39:50 +0000

    wrote a C program that tells whether the last number of a random number is greater or less than 5 and 6 or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	/*for numbers less than 6*/
-	else if (lastDigit < 6)
+	else if (lastDigit < 6 && lastDigit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
 		lastDigit);
