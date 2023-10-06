ea2ddf921bab07080d554d5c64e1ca6bb14799f2
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-06 00:24:59 +0000

    wrote a c program that prints a sting using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using the printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
