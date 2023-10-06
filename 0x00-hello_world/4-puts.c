5fd1758c7bcd4ce0a681bb1816ba0be3786b333f
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-06 00:03:55 +0000

    a c file that prints a string using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string with the puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
