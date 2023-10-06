3e7f00cea3dd43be52fc6a12c1321da9e92846b3
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-06 02:18:06 +0000

    wrote a C program that prints a string and returns 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints a string using fputs
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2025-10-19\n",
+	stdout);
+
+	return (1);
+}
