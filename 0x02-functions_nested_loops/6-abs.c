62929ef7cf81beb0458a838703ed4c7a87f7e11c
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-10 19:12:31 +0000

    a prototype that calculates the absolute value of a given integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * _abs - Calculates the absolute value of an interger
+ * @n: The given integer to be calculated
+ *
+ * Return: The value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
