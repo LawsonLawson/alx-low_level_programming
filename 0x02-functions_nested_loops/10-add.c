f92e5b713c73ac20526dd2b49d38336d35767974
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-10 20:31:45 +0000

    a C prototype file that adds two given numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * add - Adds 2 given integers
+ * @num1: The first integer in the operation
+ * @num2: The second integer in the operation
+ *
+ * Return: The result of the operation
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
