75ae7d69aa34cf34bfac62543435a002feb24af6
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-13 20:32:05 +0000

    a C program that prints integers using a special funtion _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	int number = n;
+
+
+	if (number < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
-	if (n / 10 > 0)
+	if (number / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
-	_putchar((n % 10) + 48);
+	_putchar((number % 10) + 48);
 }
