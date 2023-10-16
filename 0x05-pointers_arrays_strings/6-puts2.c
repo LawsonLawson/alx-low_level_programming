74f1106a8890d1a8620e8d465c451c5d2b387f71
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-16 21:11:53 +0000

    a C function prototype that prints every other character of a string starting with the first index

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - This function prints every other character of a string starting with
+ * the first character
+ * @str: The string character we intend to print
+ *
+ * Return: 0 (success)
+ */
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n = n + 2;
+	}
+	_putchar('\n');
+}
