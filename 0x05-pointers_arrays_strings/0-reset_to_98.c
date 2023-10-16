228043ebf82c9a226dd286662b7d7829a2c80e3d
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-16 19:29:29 +0000

    a function prototype that updates the value of the parameter passed into the function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This program resets the value of the parameter passed in it
+ * @n: the pointer or argument to which we are updating the value
+ *
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
