78465d5dbce1140410334008db91e1827f35831b
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-25 18:37:19 +0000

    any number raised to the power 0 is 1

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This function calculates the exponent of a given number by
+ * a given value
+ * @x: Our base value
+ * @y: The exponent
+ *
+ * Return: the result after the operation
+ */
+int _pow_recursion(int x, int y)
+{
+	int result;
+
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		result = x * _pow_recursion(x, (y-1));
+		return (result);
+	}
+}
