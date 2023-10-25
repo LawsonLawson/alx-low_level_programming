68c73596644e830d2db8339f353c725ab9b4f97b
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-25 14:07:03 +0000

    a walk in the park

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This function prints a string followed by a new line
+ *
+ * @s: The pointer to the first address of characters to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
