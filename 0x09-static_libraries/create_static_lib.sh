657dd57a3685100bfc73bf40196f4a2b0384b4a8
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-28 03:13:35 +0000

    reversed back to old code.. will try to be &leek later

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #changing all .c files to object code
 gcc -c *.c
-#creating static library and indexing it
-ar rc liball.a *.o && ranlib liball.a
+#creating static library
+ar rc liball.a *.o
+#indexing the library
+ranlib liball.a
