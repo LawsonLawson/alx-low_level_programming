c9c0983661426b58d48c7a44b080b1d38ff82f0c
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-28 03:10:44 +0000

    trying a &leek way to do the same thing

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #changing all .c files to object code
 gcc -c *.c
-#creating static library
-ar rc liball.a *.o
-#indexing the library
-ranlib liball.a
+#creating static library and indexing it
+ar rc liball.a *.o && ranlib liball.a
