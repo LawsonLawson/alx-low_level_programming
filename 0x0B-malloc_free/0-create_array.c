17323aee57fe2a51f71382b741e0a7d2594555d7
Lawson Pascal Israel <isreallawson@outlook.com>
2023-10-30 17:56:06 +0000

    a function that creates an array of characters & initializes it with a specific character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - This function creates an array of characters and initializes
+ * it with a specific char
+ *
+ * @size: the size of the array
+ *
+ * @c: the charcter we would assign
+ *
+ * Return: the pointer to the first character of the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	/*declaring a char pointer and a counter*/
+	char *p;
+	unsigned int n;
+
+	/*memory allocation with malloc*/
+	p = malloc(sizeof(char) * size);
+
+	/*checking if memory allocatin was a success*/
+	if (p == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+	/*assigned the characters*/
+	n = 0;
+	while (n < size)
+	{
+		p[n] = c;
+		n++;
+	}
+
+	return (p);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+
+#endif
