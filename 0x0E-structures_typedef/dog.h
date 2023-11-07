21c51091bd1d5e3562afc6c1513096e02243466d
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-07 01:53:26 +0000

    alias for struct dog added to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Generating an alias for struct dog which was defined earlier
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
