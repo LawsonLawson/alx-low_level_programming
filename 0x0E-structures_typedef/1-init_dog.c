baf43e1279bcc707556bcdfdd9daa2db0f8c8250
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-07 01:37:51 +0000

    a function that initializes a variable of a structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - This function creates a variable of type 'struct dog'
+ *
+ * @d: pointer to address of variable name
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
