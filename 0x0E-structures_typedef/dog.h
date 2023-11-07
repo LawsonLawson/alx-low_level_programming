8c4d2bca18750c13df646333dc2f16a636a2dc89
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-07 01:26:42 +0000

    a header file that defines a structure for storing info of a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - This is a data structure that contains info about a dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: whenever this structure name dog is called, we can access info
+ * about a dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
