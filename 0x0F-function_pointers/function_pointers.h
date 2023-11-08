e6e46ed06237706f420b37441603b3bff2425214
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-08 20:51:38 +0000

    I am done for today 💤

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,6 @@
 #include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
