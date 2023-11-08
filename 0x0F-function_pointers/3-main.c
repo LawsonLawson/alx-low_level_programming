2c72e326193a19e537ada00b33eb73ecbeb007ff
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-08 19:51:53 +0000

    a program of to perform simple operatiion on numbers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,30 +12,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
 	/*declaring a pointer to a function op*/
-	int (*op)(int, int);
+	int (*ptr)(int, int), n1, n2, cal;
+	char op;
 
-	if (argc != 4)
+	if (argc > 4 || argc < 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1])
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	op = *argv[2];
+	if ((op == '%' || op == '/') && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	ptr = get_op_func(argv[2]);
+	if (!ptr)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	op = get_op_func(argv[2]);
-	if (op == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", op(a, b));
+	cal = ptr(n1, n2);
+	printf("%d\n", cal);
 
 	return (0);
 }
