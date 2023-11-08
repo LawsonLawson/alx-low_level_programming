8938c46df634aab07569a865fd030192785649b7
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-08 18:37:28 +0000

    a function that searches for and integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,33 +11,21 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	/*declaring i for index and a counter*/
+	/*declaring i for index*/
 	int i;
-	int j;
 
-	if (size <= 0)
+	if (array && cmp)
 	{
-		return (-1);
-	}
-	if (!cmp || !cmp)
-	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		j = cmp(array[i]);
+		if (size <= 0)
 		{
-			if (j != 0)
-			{
-				break;
-			}
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
 		}
 	}
-	if (j == 0)
-	{
-		return (-1);
-	}
-
-	return (i);
+	return (-1);
 }
 
