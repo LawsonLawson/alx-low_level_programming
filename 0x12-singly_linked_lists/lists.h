86051157d317945e6cec103dd7a1cab9172456a8
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-16 20:00:40 +0000

    added a header file containing prototypes to functions I will need

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <sldlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string,
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function protoypes */
+unsigned int _strlen(const char *s);
+size_t print_list(const list_t *h);
+
+
+
+#endif /* LISTS_H */
