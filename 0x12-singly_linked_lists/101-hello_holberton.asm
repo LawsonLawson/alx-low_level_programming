742908e9ad18e978cedfc8513f0585d1e719ad09
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-16 22:31:26 +0000

    printing a message with assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
-extern printf
+extern printf ; Declare the printf function
 
 section .text
-global main
+global main ; Declare the entry point of the program 'main'
 
 main:
-push rbp
+push rbp ; Save the base pointer to the stack, preparing for function prologue
 
-mov rdi, fmt
-mov rsi, msg
-mov rax, 0
-call printf
+mov rdi, fmt ; First argument; Format specifier
+mov rsi, msg ; Second argument; Address of the message
+mov rax, 0 ; clear the RAX register
+call printf ; call the printf function
 
-pop rbp
+pop rbp ; Restore the base pointer, undoing the function prologue
 
-mov rax, 0
-ret
+mov rax, 0 ; Set the return value in FAX to 0 (successfull program)
+ret ; Return from 'main'
 
 section .data
-msg: db "Hello, Holberton", 0
-fmt: db "%s", 10, 0
+msg: db "Hello, Holberton", 0 ; Null-terminated string
+fmt: db "%s", 10, 0 ; Format specifier "%s" followed by '\n' and '\0'
