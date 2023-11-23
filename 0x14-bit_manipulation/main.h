92be4c1f57c11c55c874d8d7e26c0213d339b42e
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-23 15:20:11 +0000

    2 prototypes, 1 to check for a valid sting, and the other to do the convertion

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+
+/* Function prototypes */
+int _putchar(char c);
+int check_string(const char *b);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
