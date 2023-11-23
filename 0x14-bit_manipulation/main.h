04e577567599e00732199d2e7b5835d82b524a19
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-23 15:37:03 +0000

    added function prototype for a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 int check_string(const char *b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
