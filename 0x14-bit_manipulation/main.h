cd8512ae2871ec0943b25606180bd846e25e6cca
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-23 15:48:30 +0000

    a function that sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int check_string(const char *b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
