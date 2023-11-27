f0aa6701eb47ab139b5dab1e0146db1924f93fa4
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-27 17:49:00 +0000

    add header file containing neccessary header files and function prototype for a funcition to read a text and print it to stdout

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
