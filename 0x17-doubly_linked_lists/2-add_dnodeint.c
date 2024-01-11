4fde5e73adffdb8f5695cfc522ff6208badc3cd0
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-11 19:35:25 +0000

    a function that adds a new node at the beggining of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ *
+ * @head: Pointer to the head node.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
+/**
+ * add_dnodeint - Adds a node at th beggining of a dlistint.
+ *
+ * @head: Pointer to the head node.
+ *
+ * @n: Element of the new node.
+ *
+ * Return: Address of the new node(success), or NULL(failure).
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return NULL;
+	/* Set the date pointers for the new node */
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	/* Update the head to point to the new node */
+	*head = new_node;
+
+	/* Return the address of the new node */
+	return (new_node);
+}
