cf718a411cb9a58e4ddffcd8057c3309b57b088e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-01 18:44:30 +0000

    added func prototype for a function that prints a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -46,4 +46,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
+void hash_table_print(const hash_table_t *ht);
+
 #endif /* _HASH_TABLES_ */
