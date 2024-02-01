33aee25b5104db0e360c630a4a9f7f261fe72123
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-01 17:33:32 +0000

    added func prototype for function that gives index of a key

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 /* URL - https://gist.github.com/papamuziko/7bb52dfbb859fdffc4bd0f95b76f71e8 */
 unsigned long int hash_djb2(const unsigned char *str);
 
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
 #endif /* _HASH_TABLES_ */
