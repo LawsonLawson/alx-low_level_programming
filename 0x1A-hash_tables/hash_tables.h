0b05df8005d00ebe127aba76e0fd55204bc57787
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-01 19:15:08 +0000

    added func prototype to function that🚮 deletes a he

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -48,4 +48,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 void hash_table_print(const hash_table_t *ht);
 
+void hash_table_delete(hash_table_t *ht);
+
 #endif /* _HASH_TABLES_ */
