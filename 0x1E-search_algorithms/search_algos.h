dbf66ab82e31778256d0569f820781f8296a91b8
Lawson Pascal Israel <isreallawson@outlook.com>
2024-05-06 16:59:32 +0000

    added function prototype for task 9

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int binary_search(int *array, size_t size, int value);
 
 /* Advanced Tasks */
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
