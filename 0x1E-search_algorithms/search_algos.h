6fcdb362df9b455dc53e0ddf43a83e7d3cb63283
Lawson Pascal Israel <isreallawson@outlook.com>
2024-05-07 17:48:28 +0000

    consitency ...

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <stdlib.h>
 
+
 /* Data Structure for Task 12 */
 /**
  * struct listint_s - singly linked list
@@ -42,9 +43,11 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+
 /* Function protoypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
 
 /* Advanced Tasks */
 int jump_search(int *array, size_t size, int value);
@@ -53,5 +56,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
